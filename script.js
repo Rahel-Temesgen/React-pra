88580f41f3bfa5ec643f3c4bea9a66b4a73a3de7
Rahel-Temesgen <racheltemesgen13716@gmail.com>
2025-11-03 18:35:48 +0300

    testing JSX using Babel

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ const listOfUnorderedList = React.createElement('ul', null, [
     React.createElement('li', null ,'item2'),
 ])
 
+const jsxElement = (
+    <ul>
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+    </ul>
+)
+
 // ReactDOM.render('Hello world!', reactContentRoot);
 // ReactDOM.render(firstElement, reactContentRoot);
 // ReactDOM.render(unorderedList, reactContentRoot);
-ReactDOM.render(listOfUnorderedList, reactContentRoot);
+// ReactDOM.render(listOfUnorderedList, reactContentRoot);
+ReactDOM.render(jsxElement, reactContentRoot);
 
 
 
