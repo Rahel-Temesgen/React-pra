b4fa69c998b8b8e00c5890b272db2e6b71e84c3c
Rahel-Temesgen <racheltemesgen13716@gmail.com>
2025-11-07 22:58:53 +0300

    organizing react folder

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './Components/App'
-import "./Styling/global.css"
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
