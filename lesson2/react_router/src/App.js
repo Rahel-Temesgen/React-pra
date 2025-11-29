6521a7d01a5b5672909b60787ec137ce9ba85331
Rahel-Temesgen <racheltemesgen13716@gmail.com>
2025-11-30 02:31:36 +0300

    chapter 16, 17 18

---
+++ lesson2/react_router/src/App.js
@@ -0,0 +1,75 @@
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [postBody, setPostBody] = useState('');
+  const [postTitle, setPostTitle] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = () => {
+    const id = posts.length ? posts[posts.length -1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = {id, title: postTitle, datetime, body: postBody};
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    history.push('/');
+  }
+
+  const handleDelete = (id) => {
+    const postsList = posts.filter(post => post.id !== id);
+    setPosts(postsList);
+    history.push('/');
+  }
+
+  useEffect(() => {
+    const filteredResults = posts.filter(post => (
+      (post.title).toLowerCase().includes(search.toLowerCase())
+      || (post.body).toLowerCase().includes(search.toLowerCase())
+    ));
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  return (
+    <div className="App">
+      <Header title="React JS Blog"/>
+      <Nav search={search} setSearch={setSearch} />
+      <Switch>
+        <Route exact path='/'>
+          <Home posts={searchResults} />
+        </Route>
+        <Route exact path='/post'>
+          <NewPost 
+            handleSubmit={handleSubmit}
+            postBody={postBody}
+            setPostBody={setPostBody}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+          />
+        </Route>
+        <Route path='/post/:id'>
+          <PostPage posts={posts} handleDelete={handleDelete}/>
+        </Route>
+        <Route path='/about' component={About} />
+        <Route path='*' component={Missing} />
+      </Switch>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
