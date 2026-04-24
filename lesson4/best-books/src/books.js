10691c437c3f085e0dd4805d92946e4c3ed8f1ee
Rahel-Temesgen <racheltemesgen13716@gmail.com>
2026-04-24 07:28:59 +0300

    add 'Amazong best Sellers' project

---
+++ lesson4/best-books/src/books.js
@@ -0,0 +1,26 @@
+import book1 from './images/book-1.jpg'
+import book2 from './images/book-2.jpg'
+import book3 from './images/book-3.jpg'
+
+export const books = [
+  {
+    id: 1,
+    title: 'The Dinner Party',
+    author: 'Freida McFadden',
+    img: book1,
+  },
+  {
+    id: 2,
+    title: 'Judge Stone',
+    author: 'James Patterson',
+    img: book2,
+  },
+  {
+    id: 3,
+    title: 'Project Hail Mary: A Novel ',
+    author: 'Andy Weir',
+    img: book3,
+  },
+]
+
+// export default books
